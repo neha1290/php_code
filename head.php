a8f7865318b33d570d74e1545a01cf9df9be2ac5
unknown <preetisinghthakur22@gmail.com>
2020-01-03 17:44:09 +0530

    new changes

---
+++ head.php
@@ -0,0 +1,19 @@
+<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="js/slick/slick.css">
+	<link href="img/icons/logo.png" rel="icon">
+	<link rel="stylesheet" type="text/css" href="animation/animate.css">
+	<link rel="stylesheet" href="../use.fontawesome.com/releases/v5.3.1/css/all.css"
+		integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+		<link rel="stylesheet" type="text/css" href="enquiryform.css" />
+<link rel="stylesheet" href="http://code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css">
+  <script src="http://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="http://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <script
+  src="https://code.jquery.com/jquery-3.4.1.js"
+  integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
+  crossorigin="anonymous"></script>
+  <script
+  src="https://code.jquery.com/jquery-3.3.1.js"
+  integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+  crossorigin="anonymous"></script>
+  
